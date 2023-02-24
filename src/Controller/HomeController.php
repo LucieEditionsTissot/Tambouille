1f23d35ad1aa24506c2b398c31cc205fde540616
Lucas Leperlier <lucas.leperlier@gmail.com>
2023-02-24 19:08:03 +0100

    wip, require twig component, start styling and redirect things

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,15 +5,15 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
-#[Route('/', name: 'app_home')]
+#[Route('/home', name: 'app_home')]
 class HomeController extends AbstractController
 {
     #[Route('', name: '_index')]
     public function index(): Response
     {
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
         ]);
     }
 }
