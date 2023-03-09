2df27e67aaeb4a116a664d737ed3220161a15719
llesnier <llesnier@editions-tissot.fr>
2023-03-09 08:42:12 +0100

    Add group creation without front

--- src/Controller/GroupController.php
+++ src/Controller/GroupController.php
@@ -3,17 +3,23 @@
 namespace App\Controller;
 
 
+use App\Entity\Group;
+use App\Form\GroupFormType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/group', name: 'group_')]
-class GroupController
+class GroupController extends AbstractController
 {
     #[Route('/add', name: 'add')]
-    public function index(): Response
+    public function addGroup(): Response
     {
+        $group = new Group();
+        $form = $this->createForm(GroupFormType::class, $group);
+
         return $this->render('group/formGroup.html.twig', [
-            'controller_name' => 'GroupController',
+            'groupForm' => $form->createView(),
         ]);
     }
 }
