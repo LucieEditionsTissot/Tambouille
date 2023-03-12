a264cd7fed8226388a53ae791a88edf2c1484465
llesnier <llesnier@editions-tissot.fr>
2023-03-12 09:07:42 +0100

    start to do Recipe form

---
+++ src/Controller/RecipeController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Recipe;
+use App\Entity\RecipeType;
+use App\Form\GroupFormType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RecipeController extends AbstractController
+{
+    #[Route('/recipe', name: 'app_recipe')]
+    public function index(): Response
+    {
+        return $this->render('recipe/index.html.twig', [
+            'recipeForm' => 'RecipeController',
+        ]);
+    }
+
+    public function addRecipe(Request $request) :Response {
+        $recipe = new Recipe();
+        $form = $this->createForm(Group::class, $recipe);
+        $form->handleRequest($request);
+
+        return $this->render('recipe/index.html.twig', [
+            'recipeForm' => $form->createView(),
+        ]);
+    }
+}
---
+++ src/Form/RecipeFormType.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Equipement;
+use App\Entity\Group;
+use App\Entity\RecipeType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RecipeFormType
+{
+
+    private EntityManagerInterface $entityManager;
+
+    public function buildForm(Request $request, FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class)
+            ->add('cookingTime', IntegerType::class)
+            ->add('images', FileType::class)
+            ->add('nbPersons', IntegerType::class)
+            ->add('author', ChoiceType::class, [
+                'choices' => $this->getUser()->getAuthors(),
+                'choice_label' => function($author) {
+                    return $author->getFullName();
+                },
+                'choice_value' => function($author) {
+                    return $author->getId();
+                },
+            ])
+            ->add('user_id', HiddenType::class, [
+                'users' => $this->getUser()->getId(),
+            ])
+            ->add('recipeType', IntegerType::class)
+            ->add('equipementsType', ChoiceType::class, [
+                'choices' => $this->getEquipementsChoices(),
+                'multiple' => true,
+                'expanded' => true,
+            ])
+            ->add('recipeType', RecipeType::class);
+        
+    }
+    private function getEquipementsChoices(): array
+    {
+        $equipements = $this->entityManager->getRepository(Equipement::class)->findAll();
+        $choices = [];
+        foreach ($equipements as $equipement) {
+            $choices[$equipement->getName()] = $equipement->getId();
+        }
+        return $choices;
+    }
+
+
+}
