c7262def8ffede9c6a27ece4a118012b39e6290f
LucieEditionsTissot <87359344+LucieEditionsTissot@users.noreply.github.com>
2023-03-15 20:26:17 +0100

    Feat - Recipe - #13,#14,#15,#16
    Recipe create, edit, show and delete OK

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -3,16 +3,19 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use http\Env\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\CurrentUser;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class LoginController extends AbstractController
 {
-    #[Route('', name: 'app_login')]
-    public function index(AuthenticationUtils $authenticationUtils, #[CurrentUser] ?User $user): Response
+    #[Route('/login', name: 'app_login')]
+    public function index(AuthenticationUtils $authenticationUtils, #[CurrentUser] ?User $user, SessionInterface $session): Response
+
     {
         $error = $authenticationUtils->getLastAuthenticationError();
         $lastUsername = $authenticationUtils->getLastUsername();
@@ -20,7 +23,6 @@ class LoginController extends AbstractController
         if($user){
             return $this->redirectToRoute('app_feed');
         }
-
         return $this->render('login/index.html.twig', [
             'last_username' => $lastUsername,
             'error' => $error,
@@ -31,5 +33,4 @@ class LoginController extends AbstractController
     public function logout()
     {
     }
-
 }
---
+++ src/Controller/RecipeController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Group;
+use App\Entity\Image;
+use App\Entity\Recipe;
+use App\Entity\User;
+use App\Form\RecipeFormType;
+use App\Repository\RecipeRepository;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use http\Message;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use function PHPUnit\Framework\throwException;
+use const App\Entity\GROUP_ID;
+
+#[Route('/recipe', name: 'recipe_')]
+class RecipeController extends AbstractController
+{
+    #[Route('/list', name: 'list')]
+    public function index(Request $request,EntityManagerInterface $entityManager, ?User $user): Response
+    {
+        $session = $request->getSession();
+        $groupId = $session->get(GROUP_ID);
+        if(!$groupId){
+
+        }else{
+            $group = $this->getGroupById($entityManager, $groupId);
+        }
+
+        $recipe = $this->find($entityManager, $group);
+        return $this->render('recipe/listRecipe.html.twig', [
+            'hasGroup'=>$user->hasAtLeastOneGroup() and isset($group),
+            "group"=>$group,
+            "recipes"=>$recipe
+        ]);
+    }
+
+
+    private function getGroupById(EntityManagerInterface $entityManager, string $id){
+        return $entityManager->getRepository(Group::class)->findOneBy(
+            array('id'=>$id)
+        );
+    }
+
+    #[Route('/add', name: 'add')]
+    public function addRecipe(Request $request, EntityManagerInterface $entityManager, SluggerInterface $slugger): Response
+    {
+
+        $recipe = new Recipe();
+        $groupId = $request->getSession()->get(GROUP_ID);
+        $group = $this->getGroupById($entityManager, $groupId);
+        $recipe->setGroupId($group);
+        $user = $this->getUser();
+        if ($user instanceof User) {
+            $recipe->setAuthor($user);
+        }
+        $form = $this->createForm(RecipeFormType::class, $recipe);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $entityManager->getRepository(Recipe::class);
+            $imageFile = $form->get('image')->getData();
+
+            if ($imageFile) {
+                $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename . '-' . uniqid() . '.' . $imageFile->guessExtension();
+
+                try {
+                    $imageFile->move(
+                        $this->getParameter('images_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    // ... handle exception if something happens during file upload
+                }
+                $images = [$newFilename]; // Wrap the newFilename in an array
+                $recipe->setImages($images);
+            }
+            $entityManager->persist($recipe);
+            dump($recipe);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('recipe_list');
+        }
+        return $this->render('recipe/recipeForm.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
+
