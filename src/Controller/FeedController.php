ca51165ced41dc2458eaf952fb7bbf07b52a3fa2
Lucas Leperlier <71217820+dev-Sph1nX@users.noreply.github.com>
2023-03-16 00:18:09 +0100

    Feat - Comment  (#40)
    * Add comment & view & form, can delete, can edit, can view comments from other users

--- src/Controller/FeedController.php
+++ src/Controller/FeedController.php
@@ -31,6 +31,7 @@ class FeedController extends AbstractController
         }
 
         $posts = $this->findPosts($entityManager, $group);
+//        dd($posts[0]->getComments()->getValues());
         return $this->render('feed/index.html.twig', [
             'hasGroup'=>$user->hasAtLeastOneGroup() and isset($group),
             "group"=>$group,
@@ -54,7 +55,7 @@ class FeedController extends AbstractController
     private function findPosts(EntityManagerInterface $entityManager, Group $group): array
     {
         return $entityManager->getRepository(Post::class)->findBy(
-            array('groupId'=>$group->getId())
+            array('groupId'=>$group->getId(), 'parent'=>null)
         );
     }
 
