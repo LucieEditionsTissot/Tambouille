7f943d6d28a6b014f82a84b453823f7cda6b8e36
llesnier <llesnier@editions-tissot.fr>
2023-03-15 21:16:40 +0100

    redo recipe logical because of my revert

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -96,5 +96,74 @@ class RecipeController extends AbstractController
             'form' => $form->createView(),
         ]);
     }
+
+
+    #[Route('/{id}/edit', name: 'edit')]
+    public function editRecipe(Request $request, EntityManagerInterface $entityManager, SluggerInterface $slugger, int $id): Response
+    {
+        $recipe = $entityManager->getRepository(Recipe::class)->find($id);
+
+        if (!$recipe) {
+            throw $this->createNotFoundException('Recipe not found');
+        }
+
+        $form = $this->createForm(RecipeFormType::class, $recipe);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $imageFile = $form->get('image')->getData();
+
+            if ($imageFile) {
+                $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename . '-' . uniqid() . '.' . $imageFile->guessExtension();
+
+                try {
+                    $imageFile->move(
+                        $this->getParameter('images_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    // ... handle exception if something happens during file upload
+                }
+                $images = [$newFilename]; // Wrap the newFilename in an array
+                $recipe->setImages($images);
+            }
+
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_cooking_book');
+        }
+
+        return $this->render('recipe/recipeForm.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/{id}/show', name: 'show')]
+    public function showRecipe(Recipe $recipes): Response
+    {
+        return $this->render('recipe/show.html.twig', [
+            'recipes' => $recipes,
+        ]);
+    }
+
+    #[Route('/{id}/delete', name: 'delete')]
+    public function deleteRecipe(Request $request, EntityManagerInterface $entityManager, int $id): Response
+    {
+        $recipe = $entityManager->getRepository(Recipe::class)->find($id);
+
+        if (!$recipe) {
+            throw $this->createNotFoundException('Nous n\'avons pas trouvé cette recette');
+        }
+
+        $entityManager->remove($recipe);
+        $entityManager->flush();
+
+        $this->addFlash('success', 'Votre recette a bien été supprimé');
+
+        return $this->redirectToRoute('app_cooking_book');
+    }
 }
+
 
