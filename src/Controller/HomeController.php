eed7efca4fffe06f3f9195ee762601e4d5d25463
Lucas Leperlier <lucas.leperlier@gmail.com>
2023-03-17 23:12:41 +0100

    post, comment and new recipe notification styles

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,7 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
-#[Route('/home', name: 'app_home')]
+#[Route('', name: 'app_home')]
 class HomeController extends AbstractController
 {
     #[Route('', name: '_index')]
