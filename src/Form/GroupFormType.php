4b74500adde1f479d736a2bb4b86886d1eb40826
Roxane-gll <guella.roxane@gmail.com>
2023-03-18 11:09:47 +0100

    Add style to create group

--- src/Form/GroupFormType.php
+++ src/Form/GroupFormType.php
@@ -13,9 +13,16 @@ class GroupFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name', TextType::class)
+            ->add('name', TextType::class, [
+                'attr'=> ['style'=> 'background-color: rgba(229,221, 205,50%); border: none'],
+                'label' => 'Nom de groupe'
+            ])
             ->add('code', TextType::class, array(
                 'disabled' => true,
+                'attr'=> [
+                    'style'=> 'background-color: rgba(229,221, 205,50%); border: none; width: fit-content',
+                    ],
+                'label' => 'Code groupe'
             ));
     }
 
